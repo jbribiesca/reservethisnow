0cfac77ad8aecfefb62155d72abfefd22f75522f
Jose Torres <jt227b@gmail.com>
2019-07-12 12:24:47 -0500

    Added footer to scheduele app

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -24,13 +24,13 @@ const Nav = (props) => {
   }
   
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark">
       <Col size="md-2">
         <Link to="/" className="brandName">Reserve This Now</Link>
       </Col>
       <Col size="md-7"></Col>
       <Col size="md-3">
-        <div className="float-right">
+        <div className="float-right-nav">
         {greeting} - <Link to="#" className="logout" onClick={props.logout}>Logout</Link>
         </div>
       </Col>
